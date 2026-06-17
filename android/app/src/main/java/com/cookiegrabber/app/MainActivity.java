a0a32159e57e0c2f56ba3faf45f134bec9a5ccf5
PadhleYrr <moliage490@gmail.com>
2026-06-17 21:40:14 +0530

    Add files via upload

--- android/app/src/main/java/com/cookiegrabber/app/MainActivity.java
+++ android/app/src/main/java/com/cookiegrabber/app/MainActivity.java
@@ -2,7 +2,10 @@ package com.cookiegrabber.app;
 
 import android.os.Bundle;
 import android.webkit.CookieManager;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -11,15 +14,41 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Enable cookies & 3rd party cookies on the WebView
+        WebView webView = getBridge().getWebView();
+
+        // Enable cookies & 3rd party cookies
         CookieManager cookieManager = CookieManager.getInstance();
         cookieManager.setAcceptCookie(true);
-        cookieManager.setAcceptThirdPartyCookies(getBridge().getWebView(), true);
+        cookieManager.setAcceptThirdPartyCookies(webView, true);
 
         // Inject native cookie bridge into the JS context
-        getBridge().getWebView().addJavascriptInterface(
+        webView.addJavascriptInterface(
             new CookieBridge(cookieManager),
-            "AndroidCookies"   // accessible as window.AndroidCookies in JS
+            "AndroidCookies"
+        );
+
+        // Strip headers that block WebView loading (X-Frame-Options, CSP)
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                WebResourceResponse response = super.shouldInterceptRequest(view, request);
+                if (response != null && response.getResponseHeaders() != null) {
+                    java.util.Map<String, String> headers = new java.util.HashMap<>(response.getResponseHeaders());
+                    headers.remove("X-Frame-Options");
+                    headers.remove("x-frame-options");
+                    headers.remove("Content-Security-Policy");
+                    headers.remove("content-security-policy");
+                    response.setResponseHeaders(headers);
+                }
+                return response;
+            }
+        });
+
+        // Enable JS, DOM storage, mixed content
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setMixedContentMode(
+            android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
         );
     }
 }
